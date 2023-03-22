9c439d28a6b9094ac0c86d7ecd32464d2510c571
Huang <794821788@qq.com>
2023-03-22 16:47:33 +0800

    Merge branch 'hot-fix'
    # Conflicts:
    #	src/main/java/com/huang/git.java

--- src/main/java/com/huang/git.java
+++ src/main/java/com/huang/git.java
@@ -7,5 +7,6 @@ public class git {
         System.out.println("Hello Git3");
         System.out.println("Master test!");
         System.out.println("Hot-fix test!");
+        System.out.println("Hot-fix test!");
     }
 }
